d0b4b1deb10d22bea35e5dffeea366401a95237d
adityatulsyan03 <tulsyanaditya03@gmail.com>
2025-04-27 03:09:01 +0530

    Initial Commit

---
+++ src/main/java/com/example/BasicAuth/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.BasicAuth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String Greet(HttpServletRequest request) {
+        return "Hello World\n" + request.getSession().getId();
+    }
+
+}
